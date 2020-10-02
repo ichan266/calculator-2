e7e658468a740dd1539a0ccabae058e7614eb103
Iris Kuhn <Iris@Iris-PC.kuponet.local>
2020-10-01 22:11:44 -0700

    final version

--- calculator.py
+++ calculator.py
@@ -3,23 +3,39 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
-# Replace this with your code
-
-#repeat forever:
 while True:
-#    read input
-    user_input = input("Please enter an equation :) >")
-#    tokenize input
-    tokenize = user_input.split(" ")
-#        if the first token is "q":
-    if "q" in tokenize:
-        print("That was fun, see ya later!")
+    user_input = input("Please enter an equation (or enter q to quite) > ").lower()
+    token = user_input.split()
+    if "q" in token:
+        print("Exiting Program")
         break
-#            quit
 #            (decide which math function to call based on first token)
-    if tokenize[0] == 'pow':
-        return tokenzie[1] ** tokenize[2]                                               
 
-#           if the first token is 'pow':
-#                  call the power function with the other two tokens#
+    num1 = int(token[1])
+
+    if len(token) == 3:
+        num2 = int(token[2])
+
+    if token[0] == "+":
+        print(add(num1, num2))
+
+    elif token[0] == "-":
+        print(subtract(num1,num2))
+
+    elif token[0] == "x":
+        print(multiply(num1,num2))
+
+    elif token[0] == "/":
+        print(divide(num1,num2))
+
+    elif token[0] == "square":
+        print(square(num1))
+
+    elif token[0] == "cube":
+        print(cube(num1))
+
+    elif token[0] == "power":
+        print(power(num1, num2))
+    
+    elif token[0] == "mod":
+        print(mod(num1, num2))
